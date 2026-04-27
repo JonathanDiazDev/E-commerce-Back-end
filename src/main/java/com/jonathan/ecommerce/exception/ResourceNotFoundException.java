9ec96570588d0448861d92e379ff4eef53f7eea2
jonathandiaz.dev@gmail.com <jonathandiaz.dev@gmail.com>
2026-04-27 12:57:52 -0600

    refactor(exception): improve custom exception structure

--- src/main/java/com/jonathan/ecommerce/exception/ResourceNotFoundException.java
+++ src/main/java/com/jonathan/ecommerce/exception/ResourceNotFoundException.java
@@ -1,7 +1,7 @@
 package com.jonathan.ecommerce.exception;
 
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String message){
-        super(message);
-    }
+public class ResourceNotFoundException extends RuntimeException {
+  public ResourceNotFoundException(String message) {
+    super(message);
+  }
 }
